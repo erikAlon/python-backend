05865b201db7a1b6339d432904c4fec3af8648a4
erikalon <mr.alonsoea@protonmail.ch>
2018-07-12 17:27:31 -0500

    added personalnotes get/ post implementation to admin page

--- notes/api.py
+++ notes/api.py
@@ -1,8 +1,8 @@
 from rest_framework import serializers, viewsets
-from .models import Notes
+from .models import Notes, PersonalNotes
 
 
-class NotesSerializer(serializers.HyperlinnkedModelSerializer):
+class NotesSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Notes
         fields = ('title', 'content')
@@ -11,3 +11,23 @@ class NotesSerializer(serializers.HyperlinnkedModelSerializer):
 class NotesViewset(viewsets.ModelViewSet):
     serializer_class = NotesSerializer
     queryset = Notes.objects.all()
+
+
+class PersonalNotesSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = PersonalNotes
+        fields = ('title', 'content')
+
+    def create(self, validated_data):
+        # import pdb
+        # pdb.set_trace()
+        user = self.context['request'].user
+        personal_note = PersonalNotes.objects.create(
+            user=user, **validated_data)
+        return personal_note
+        # pass
+
+
+class PersonalNotesViewset(viewsets.ModelViewSet):
+    serializer_class = PersonalNotesSerializer
+    queryset = PersonalNotes.objects.all()
